2fe600c03ddfeb582f9204c272fec81f8d2ee939
brice-morin <brice.morin1@gmail.com>
2013-06-21 14:56:14 +0200

    prepare autoscalar bridge

---
+++ misc/autoscalar/src/main/java/eu/remics/autoscalar/Bridge.java
@@ -0,0 +1,11 @@
+package eu.remics.autoscalar;
+
+import org.cloudml.core.DeploymentModel;
+
+class Bridge {
+    
+    public static void toXML(DeploymentModel model, String path) {
+        
+    }
+    
+}
