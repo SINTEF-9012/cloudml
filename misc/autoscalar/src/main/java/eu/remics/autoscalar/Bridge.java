e21d27320d371f5062c9e8f712a7a71ba90eeadc
brice-morin <brice.morin1@gmail.com>
2013-06-21 17:35:55 +0200

    initial version of autoscalar bridge

--- misc/autoscalar/src/main/java/eu/remics/autoscalar/Bridge.java
+++ misc/autoscalar/src/main/java/eu/remics/autoscalar/Bridge.java
@@ -1,12 +1,15 @@
 package eu.remics.autoscalar;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
+import org.cloudml.codecs.JsonCodec;
+import org.cloudml.codecs.commons.Codec;
+import org.cloudml.core.CloudMLElement;
 import org.cloudml.core.DeploymentModel;
+import org.cloudml.core.Node;
 
 class Bridge {
 
@@ -15,20 +18,61 @@ class Bridge {
     public static void toXML(DeploymentModel model, String path) {
         try {
             String config = FileUtils.readFileToString(new File(bridge.getClass().getResource("/config.xml").toURI()));
-            String component = FileUtils.readFileToString(new File(bridge.getClass().getResource("/component.xml").toURI()));
-            String group = FileUtils.readFileToString(new File(bridge.getClass().getResource("/group.xml").toURI()));
-            
+            final String component = FileUtils.readFileToString(new File(bridge.getClass().getResource("/component.xml").toURI()));
+            final String group = FileUtils.readFileToString(new File(bridge.getClass().getResource("/group.xml").toURI()));
+
+            for (Node n : model.getNodeTypes().values()) {
+                final String groupName = n.getProperty("groupName");
+                System.out.println(n.getName() + ": " + groupName);
+                if (groupName != null) {
+                    final String minServer = (n.getProperty("minServer") != null) ? n.getProperty("minServer") : "1";
+                    final String maxServer = (n.getProperty("maxServer") != null) ? n.getProperty("maxServer") : "3";
+                    final String deltaUp = (n.getProperty("deltaUp") != null) ? n.getProperty("deltaUp") : "1";
+                    final String deltaDown = (n.getProperty("deltaDown") != null) ? n.getProperty("deltaDown") : "1";
+                    final String cpuUp = (n.getProperty("cpuUp") != null) ? n.getProperty("deltaUp") : "70";
+                    final String cpuDown = (n.getProperty("cpuDown") != null) ? n.getProperty("cpuDown") : "35";
+                    
+                    String newGroup = new String(group);
+                    
+                    newGroup = newGroup.replace("<!--NAME-->", groupName);
+                    newGroup = newGroup.replace("<!--MIN_SERVERS-->", minServer);
+                    newGroup = newGroup.replace("<!--MAX_SERVERS-->", maxServer);
+                    newGroup = newGroup.replace("<!--DELTA_UP-->", deltaUp);
+                    newGroup = newGroup.replace("<!--DELTA_DOWN-->", deltaDown);
+                    newGroup = newGroup.replace("<!--CPU_UP-->", cpuUp);
+                    newGroup = newGroup.replace("<!--CPU_DOWN-->", cpuDown);
+                    
+                    config = config.replace("<!-- GROUP -->", "<!-- GROUP -->\n" + newGroup + "\n");
+                }
+            }
+
             System.out.println(config);
-            System.out.println(component);
-            System.out.println(group);
+
+            FileUtils.writeStringToFile(new File(path), config);
         } catch (URISyntaxException ex) {
             Logger.getLogger(Bridge.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Bridge.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public static void main(String args[]) {
-        toXML(null, null);
+        InputStream stream = null;
+        try {
+            Codec codec = new JsonCodec();
+            stream = new FileInputStream(new File(bridge.getClass().getResource("/ut_load_balancing_trial.json").toURI()));
+            CloudMLElement model = codec.load(stream);
+            toXML((DeploymentModel)model, "component_" + System.currentTimeMillis() + ".xml");
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(Bridge.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Bridge.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                stream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Bridge.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
