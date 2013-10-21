eab8ef5d55f6aca270e3d02e77fcb8ee9361d480
songhui <huis@SINTEFPC5681.sintef.no>
2013-10-21 16:10:10 +0200

    listen to Any changes

---
+++ mrt/coord/src/main/java/org/cloudml/mrt/coord/cmd/gen/ListenToAny.java
@@ -0,0 +1,20 @@
+package org.cloudml.mrt.coord.cmd.gen;
+
+import org.cloudml.mrt.coord.cmd.abstracts.Change;
+import org.cloudml.mrt.coord.cmd.abstracts.Listener;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+
+@SuppressWarnings("all")
+public class ListenToAny extends Listener {
+  public ListenToAny() {
+  }
+  
+  public ListenToAny(final Procedure1<ListenToAny> initializer) {
+    initializer.apply(this);
+  }
+  
+  @Override
+  protected boolean _careFor(final Change change) {
+    return true;
+  }
+}
