9fe08341717991a00f2671f2faa129ba8f7bf098
nicolasferry <nicolas.ferry@sintef.no>
2014-01-15 11:28:55 +0100

    Update edition part + include alert message

--- ui/webeditor/src/main/webapp/js/demo.js
+++ ui/webeditor/src/main/webapp/js/demo.js
@@ -405,3 +405,36 @@ function loadDeploymentModel(jsonString) {
 	});
 
 }
+
+//Alert Messages
+function alertMessage(type,message,timeout) {
+	alertDiv = $(document.createElement('div'));
+
+	switch (type) {
+		case "success":
+			alertDiv.attr("class","alert alert-success fade in")
+					.html("<b>Success.</b> "+message);
+			break;
+		case "error":
+			alertDiv.attr("class","alert alert-error fade in")
+					.html("<b>Error.</b> "+message);
+			break;
+		case "warning":
+			alertDiv.attr("class","alert fade in")
+					.html("<b>Warning.</b> "+message);
+			break;
+		default:
+			break;
+	}
+
+	alertDiv.append(
+			$(document.createElement('a'))
+				.attr("class","close")
+				.attr("data-dismiss","alert")
+				.html("&times;")
+		);
+
+	$('#alert-div').append(alertDiv);
+	if(typeof timeout!='undefined')
+		window.setTimeout(function() { $('#alert-div').find(':contains('+message+')').remove(); }, timeout);
+}
