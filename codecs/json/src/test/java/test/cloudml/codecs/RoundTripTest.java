ee0a381b107cae2522d9d3fbed220e31db51e64d
nicolasferry <nicolas.ferry@sintef.no>
2014-02-24 15:08:32 +0100

    Prepare tests

---
+++ codecs/json/src/test/java/test/cloudml/codecs/RoundTripTest.java
@@ -0,0 +1,89 @@
+/**
+ * This file is part of CloudML [ http://cloudml.org ]
+ *
+ * Copyright (C) 2012 - SINTEF ICT
+ * Contact: Franck Chauvel <franck.chauvel@sintef.no>
+ *
+ * Module: root
+ *
+ * CloudML is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * CloudML is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with CloudML. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package test.cloudml.codecs;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import org.cloudml.codecs.commons.Codec;
+import org.cloudml.core.*;
+
+/**
+ * Unit test for rountrip testing of CloudML models.
+ */
+public class RoundTripTest extends TestCase {
+
+    File input;
+    Codec codec;
+
+    public RoundTripTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RoundTripTest(String testName, File input, Codec codec) {
+        this(testName);
+        this.input = input;
+        this.codec = codec;       
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new JsonRoundTripTestSuite();
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testRoundTrip() {
+        long start = System.currentTimeMillis();
+        if (input != null && codec != null) {
+            InputStream stream = null;
+            try {
+                stream = new FileInputStream(input);
+
+            } catch (IOException ex) {
+                Logger.getLogger(RoundTripTest.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                System.out.println("took " + (System.currentTimeMillis() - start) + " ms" );
+                try {
+                    if (stream != null) {
+                        stream.close();
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(RoundTripTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } else {
+            System.out.println("Dummy test: no codec and/or no input file");
+        }
+    }
+}
---
+++ codecs/xmi/src/test/java/test/cloudml/codecs/XMIRoundTripTestSuite.java
@@ -0,0 +1,70 @@
+/**
+ * This file is part of CloudML [ http://cloudml.org ]
+ *
+ * Copyright (C) 2012 - SINTEF ICT
+ * Contact: Franck Chauvel <franck.chauvel@sintef.no>
+ *
+ * Module: root
+ *
+ * CloudML is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * CloudML is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with CloudML. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package test.cloudml.codecs;
+
+/**
+ * This file is part of CloudML [ http://cloudml.org ]
+ *
+ * Copyright (C) 2012 - SINTEF ICT Contact: Franck Chauvel
+ * <franck.chauvel@sintef.no>
+ *
+ * Module: root
+ *
+ * CloudML is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * CloudML is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CloudML. If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.io.File;
+import java.io.FilenameFilter;
+import org.cloudml.codecs.XmiCodec;
+
+/**
+ * Unit test for rountrip testing of CloudML models.
+ */
+public class XMIRoundTripTestSuite extends RoundTripTestSuite {
+
+
+    public XMIRoundTripTestSuite() {
+        super();
+        setCodec(new XmiCodec());
+        FilenameFilter xmiFilter = new FilenameFilter() {
+
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".xmi");
+            }
+        };
+        setFilter(xmiFilter);
+        init();
+    }
+
+  
+}
