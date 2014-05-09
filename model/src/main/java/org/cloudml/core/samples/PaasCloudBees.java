d01c49e31fec53b296ae79b00249684b05ff1303
songhui <hui.song@sintef.no>
2014-05-09 09:36:07 +0200

    switch back to the PaaS example in CloudML::Model

--- model/src/main/java/org/cloudml/core/samples/PaasCloudBees.java
+++ model/src/main/java/org/cloudml/core/samples/PaasCloudBees.java
@@ -45,40 +45,49 @@ public class PaasCloudBees {
                         aProvidedPort().named("db")
                 )
                 .providedBy("CloudBees");
-
+        
         ExternalComponentBuilder wserverb = anExternalComponent()
                 .named("granny-cloudml")
                 .with(
                         aProvidedExecutionPlatform()
-                                .named("TomCat")
+                            .named("TomCat")
                 )
                 .providedBy("CloudBees");
-
+        
         InternalComponentBuilder grannyb = anInternalComponent()
                 .named("granny-war")
                 .with(aRequiredExecutionPlatform().named("TomCat"))
                 .withProperty("warfile", "C:\\temp\\granny-common.war")
                 .withProperty("temp-warfile", "C:\\temp\\granny-common-temp.war")
                 .with(aRequiredPort().remote().named("db").mandatory());
-
+        
         RelationshipBuilder relb = aRelationship()
                 .from("granny-war", "db")
                 .to("cbdb", "db")
                 .named("dbrel")
                 .withResource(
                         aResource()
-                                .named("dbconfig")
-                                .withProperty("valet", "war-xml")
-                                .withProperty("path", "WEB-INF/classes/META-INF/spring/app-context.xml")
-                                .withProperty("#getPublicAddress", "//bean[@id=\"dataSource\"]/property[@name=\"url\"]/@value")
-                                .withProperty("@#getPublicAddress-prefix", "jdbc:mysql://")
-                                .withProperty("##getLogin","//bean[@id=\"dataSource\"]/property[@name=\"username\"]/@value")
-                                .withProperty("##getPasswd","//bean[@id=\"dataSource\"]/property[@name=\"password\"]/@value")
+                            .named("dbconfig")
+                            .withProperty("valet","war-xml")
+                            .withProperty("entry_spring", "WEB-INF/classes/META-INF/spring/app-context.xml")
+                            .withProperty("path_dburl", "@self{properties/entry_spring}:://bean[@id=\"dataSource\"]/property[@name=\"url\"]/@value")
+                            .withProperty("value_dburl", "jdbc:mysql://@instance{providedEnd/owner/value/publicAddress}")
+                            .withProperty("path_dbuser", "@self{properties/entry_spring}:://bean[@id=\"dataSource\"]/property[@name=\"username\"]/@value")
+                            .withProperty("value_dbuser", "@instance{providedEnd/owner/value/type/login}")
+                            .withProperty("path_dbpassword", "@self{properties/entry_spring}:://bean[@id=\"dataSource\"]/property[@name=\"username\"]/@value")
+                            .withProperty("value_dbpassword", "@instance{providedEnd/owner/value/type/passwd}")
+//                            .withProperty("valet", "war-xml")
+//                            .withProperty("path", "WEB-INF/classes/META-INF/spring/app-context.xml")
+//                            .withProperty("#getPublicAddress", "//bean[@id=\"dataSource\"]/property[@name=\"url\"]/@value")
+//                            .withProperty("@#getPublicAddress-prefix", "jdbc:mysql://")
+//                            .withProperty("##getLogin","//bean[@id=\"dataSource\"]/property[@name=\"username\"]/@value")
+//                            .withProperty("##getPasswd","//bean[@id=\"dataSource\"]/property[@name=\"password\"]/@value")
                 );
-
-
-
+                
+                
+        
         DeploymentBuilder dmb = new DeploymentBuilder()
+                .named("cloudbees-deployment")
                 .with(aProvider()
                         .named("CloudBees")
                         .withProperty("account", "mod4cloud")
@@ -90,21 +99,20 @@ public class PaasCloudBees {
         dmb.with(anExternalComponentInstance()
                 .named("cbdb1")
                 .ofType("cbdb")
-        ).with(anExternalComponentInstance()
+            ).with(anExternalComponentInstance()
                 .named("granny-cloudml")
                 .ofType("granny-cloudml")
-        ).with(anInternalComponentInstance()
+            ).with(anInternalComponentInstance()
                 .named("granny-war-i")
                 .ofType("granny-war")
                 .hostedBy("granny-cloudml")
-        ).with(aRelationshipInstance()
+            ).with(aRelationshipInstance()
                 .named("dbreli")
                 .ofType("dbrel")
                 .from("granny-war-i", "db")
                 .to("cbdb1", "db")
-        )
-        ;
-        Deployment dm = dmb.build();
+            )
+            ;
         return dmb;
     }
 
