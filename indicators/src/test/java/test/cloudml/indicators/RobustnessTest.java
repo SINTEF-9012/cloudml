2ca2077c40bdb1e0383897a5dcde9d30dfd8bcae
Franck Chauvel <franck.chauvel@sintef.no>
2014-11-10 12:42:57 +0100

    Initial Maven structure of the 'indicators' module

---
+++ indicators/src/test/java/test/cloudml/indicators/RobustnessTest.java
@@ -0,0 +1,48 @@
+/**
+ * This file is part of CloudML [ http://cloudml.org ]
+ *
+ * Copyright (C) 2012 - SINTEF ICT
+ * Contact: Franck Chauvel <franck.chauvel@sintef.no>
+ *
+ * Module: root
+ *
+ * CloudML is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * CloudML is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with CloudML. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package test.cloudml.indicators;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Test the computation of robustness, especially the conversion from CloudML to
+ * Trio models.
+ *
+ * We assume here that Trio's robustness calculations are correct and we
+ * therefore only test for a correct generation of a Trio model out of a CloudML
+ * deployment.
+ */
+@RunWith(JUnit4.class)
+public class RobustnessTest {
+
+    @Test
+    public void sampleTest() {
+        assertThat("truth should hold", true);
+    }
+    
+}
