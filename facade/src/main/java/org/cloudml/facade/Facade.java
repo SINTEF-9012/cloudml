1136aa1cf9b605da8e8d06cc3c739046a5dd80a0
nicolasferry <nicolas.ferry@sintef.no>
2014-11-24 14:23:04 +0100

    Add providers in the diff

--- facade/src/main/java/org/cloudml/facade/Facade.java
+++ facade/src/main/java/org/cloudml/facade/Facade.java
@@ -333,6 +333,7 @@ class Facade implements CloudML, CommandHandler {
 
             deploy.getComponents().addAll(deploy2.getComponents());
             deploy.getRelationships().addAll(deploy2.getRelationships());
+            deploy.getProviders().addAll(deploy2.getProviders());
 
             deploy.getRelationshipInstances().removeAll(diff.getRemovedRelationships());
             deploy.getExecuteInstances().removeAll(diff.getRemovedExecutes());
