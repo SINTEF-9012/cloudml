9ea16f088246d7aa44259229f59c7bc5207997b0
nicolasferry <nicolas.ferry@sintef.no>
2015-03-12 21:48:32 +0100

    Prepare heroku sprint

---
+++ connectors/src/main/java/org/cloudml/connectors/HerokuConnector.java
@@ -0,0 +1,93 @@
+/**
+ * This file is part of CloudML [ http://cloudml.org ]
+ *
+ * Copyright (C) 2012 - SINTEF ICT
+ * Contact: Franck Chauvel <franck.chauvel@sintef.no>
+ *
+ * Module: root
+ *
+ * CloudML is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * CloudML is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with CloudML. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.cloudml.connectors;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import com.heroku.api.*;
+import com.heroku.api.connection.ConnectionFactory;
+
+/**
+ * Created by ferrynico on 06/03/15.
+ */
+public class HerokuConnector implements PaaSConnector {
+    private static final Logger journal = Logger.getLogger(HerokuConnector.class.getName());
+    private HerokuAPI api;
+
+
+    public HerokuConnector(String passwd){
+        api=new HerokuAPI(ConnectionFactory.get(),passwd);
+    }
+
+    @Override
+    public void createEnvironmentWithWar(String applicationName, String domainName, String envName, String stackName, String warFile, String versionLabel) {
+        App application=new App().named(applicationName);
+        api.createApp(application);
+    }
+
+    @Override
+    public void createDBInstance(String engine, String version, String dbInstanceIdentifier, String dbName, String username, String password, Integer allocatedSize, String dbInstanceClass, String securityGroup) {
+
+    }
+
+    @Override
+    public String getDBEndPoint(String dbInstanceId, int timeout) {
+        return null;
+    }
+
+    @Override
+    public void uploadWar(String warFile, String versionLabel, String applicationName, String envName, int timeout) {
+
+    }
+
+    @Override
+    public String createQueue(String name) {
+        return null;
+    }
+
+    @Override
+    public void deleteQueue(String name) {
+
+    }
+
+    @Override
+    public List<String> listQueues() {
+        return null;
+    }
+
+    @Override
+    public void restoreDB(String host, String port, String dbUser, String dbPass, String dbName, String local_file) {
+
+    }
+
+    @Override
+    public void configAppParameters(String applicationName, Map<String, String> params) {
+
+    }
+
+    @Override
+    public void bindDbToApp(String appId, String dbId, String alias) {
+
+    }
+}
