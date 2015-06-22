c74fc83194547c80d373b5df506188695ecd5346
nicolasferry <nicolas.ferry@sintef.no>
2015-06-22 17:55:39 +0200

    Serialize status too

--- deployer/src/main/java/org/cloudml/deployer/CloudAppDeployer.java
+++ deployer/src/main/java/org/cloudml/deployer/CloudAppDeployer.java
@@ -1348,9 +1348,9 @@ public class CloudAppDeployer {
 
 
     private void setEnvVar(VMInstance vmi, String varName, String value){
-        String command="echo export "+varName+"="+value+" >> ~/.bashrc";
+        //String command="echo export "+varName+"="+value+" >> ~/.bashrc";
         Connector jc = ConnectorFactory.createIaaSConnector(vmi.getType().getProvider());
-        jc.execCommand(vmi.getId(), command, "ubuntu", vmi.getType().getPrivateKey());
+        //jc.execCommand(vmi.getId(), command, "ubuntu", vmi.getType().getPrivateKey());
         String command3="sudo sh -c 'echo export "+varName+"="+value+" >> /etc/environment'";
         jc.execCommand(vmi.getId(), command3, "ubuntu", vmi.getType().getPrivateKey());
         jc.closeConnection();
